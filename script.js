2f7b3027a35972582c4ee13fea31ff277297aca4
Alvin Li <aflip24@gmail.com>
2024-01-21 01:34:05 -0600

    add cell click logic

--- script.js
+++ script.js
@@ -19,8 +19,16 @@ function handlePlayerChange() {
 
 }
 
-function handleCellClick() {
-
+function handleCellClick(clickedCellEvent) {
+    const clickedCell = clickedCellEvent.target;
+    const clickedCellIndex= parseInt(
+        clickedCell.getAttribute('data-cell-index')
+    );
+    if (gameState[clickedCellIndex] !== "" || !gameActive) {
+        return;
+    }
+    handleCellPlayed(clickedCell, clickedCellIndex);
+    handleResultValidation();
 }
 
 function handleRestartGame() {
