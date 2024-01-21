6c06624b9794d36c828f52ad88338de201084c9b
Alvin Li <aflip24@gmail.com>
2024-01-21 01:28:18 -0600

    add game logic

--- script.js
+++ script.js
@@ -6,3 +6,27 @@ let gameState = ["", "", "", "", "", "", "", "", ""];
 const winningMessage= () => `Player ${currentPlayer} has won!`;
 const drawMessage = () =>  `Game ended in a draw!`;
 const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
+
+/* initial message */
+statusDisplay.innerHTML = currentPlayerTurn();
+
+/* game logic */
+function handleCellPlayed() {
+
+}
+
+function handlePlayerChange() {
+
+}
+
+function handleCellClick() {
+
+}
+
+function handleRestartGame() {
+
+}
+
+document.querySelectorALL('.cell').forEach(cell =>
+cell.addEventListener('click', handleCellClick));
+document.querySelector('.game--restart').addEventListener('click', handleRestartGame);
